62b262cfa34c87cee790b259df71a78cf137975c
Spangberg <spabenr@dunwoody.edu>
2025-10-06 10:58:57 -0500

    initial commit

--- app.py
+++ app.py
@@ -5,4 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def benMovies():
-    
+    return render_template( 'benSite.html')
+
+
+
+@app.route('/aboutme')
+
+def aboutMe():
+    return render_template("aboutme.html")
