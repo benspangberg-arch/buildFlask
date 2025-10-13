3165c37b55d206c241a7d0ffcb5a0b2ae3312a83
Ben Spangberg <ben.spangberg@gmail.com>
2025-10-13 15:42:13 -0500

    database commit

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ from datetime import datetime, timezone
 app = Flask(__name__)
 app.secret_key = 'top-secret'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///guestlist.db'
+import os
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(BASE_DIR, 'guestlist.db')}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -13,10 +15,9 @@ class Profile(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), nullable=False)
-    quan = db.Column(db.Integer, nullable=False)
-    comments = db.Column(db.Text)
-    rel = db.Column(db.String(50), nullable=False)
-    accomodations = db.Column(db.Boolean, default=False)
+    height = db.Column(db.Integer, nullable=False)
+    pokemon = db.Column(db.Text)
+    type = db.Column(db.String(50), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.now(timezone.utc))
 
 class Feedback(db.Model):
@@ -40,12 +41,11 @@ def profile():
     if request.method == 'POST':
         name = request.form.get('name', '').strip()
         email = request.form.get('email', '').strip()
-        quan = request.form.get('quan', '').strip()
-        comments = request.form.get('comments', '').strip()
-        rel = request.form.get('rel', '').strip()
-        accommodations = request.form.get('accommodations') == "yes" # True if checked
+        height = request.form.get('height', '').strip()
+        type = request.form.get('type', '').strip()
+        pokemon = request.form.get('pokemon', '').strip()
         # Validation
-        if not name or not email or not quan or not rel:
+        if not name or not email or not height or not type:
             error = "Please fill in all required fields"
             return render_template('profileForm.html', error=error)
         
@@ -53,15 +53,15 @@ def profile():
             new_profile = Profile(
                 name=name,
                 email=email,
-                quan=int(quan),
-                comments=comments,
-                rel=rel,
-                accommodations=accommodations
+                height=int(height),
+                pokemon=pokemon,
+                type=type,
             )
             db.session.add(new_profile)
             db.session.commit()
         except Exception as e:
             db.session.rollback()
+            print("DEBUG ERROR:", e)
             error = "An error occured while saving your profile. Please try again."
             return render_template('profileForm.html', error=error)
         
@@ -70,7 +70,7 @@ def profile():
         
         
         return render_template(
-            'profileSuccess.html',name=name,email=email,quan=quan,comments=comments,rel=rel,accommodations=accommodations)
+            'profileSuccess.html',name=name,email=email,height=height,pokemon=pokemon,type=type)
     return render_template('profileForm.html')
 
         
@@ -94,18 +94,22 @@ def feedback():
             db.session.rollback()
             error = "An error occured while saving your feedback. Please try again" 
             return render_template('feedbackForm.html', error=error)
+        return render_template(
+                    'feedbackSuccess.html',
+                    rating=rating,
+                    feedback=comment_text
+                )
+    return render_template('feedbackForm.html')
 
 
 
 
 
 
-
-
-@app.route('/admin/profiles')
+@app.route('/admin/profile')
 def admin_profiles():
     profiles = Profile.query.all()
-    return render_template('Admin_profiles.html', profiles=profiles)
+    return render_template('Admin_profile.html', profiles=profiles)
 
 
 
@@ -115,10 +119,7 @@ def admin_feedback():
     return render_template('Admin_feedback.html', feedbacks=feedbacks)
 
 
-
-
-
       
     #return render_template(
-          #  'profileSuccess.html',name=name,email=email,quan=quan,comments=comments,rel=rel,accommodations=accommodations)
+          #  'profileSuccess.html',name=name,email=email,height=height,pokemon=pokemon,type=type,accommodations=accommodations)
    # return render_template('profileForm.html')
